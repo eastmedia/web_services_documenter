c782245dcbce5dab6b98429cb53c02995a08e4ee
Scott Taylor <scott@railsnewbie.com>
2011-06-08 16:25:25 -0400

    refactoring

--- web_service_documentor.rb
+++ web_service_documentor.rb
@@ -4,47 +4,63 @@ require 'json'
 require 'net/http'
 
 class WebServiceDocumentor
+  class Service
+    def initialize(base_uri, options)
+      @base_uri = base_uri
+      @endpoint = options["endpoint"]
+      @params   = options["params"]
+    end
 
-  def self.generate_documentation(path_to_web_services)
-    yml_file     = YAML.load_file(path_to_web_services)
-    web_services = yml_file["web_services"]
-    base_uri     = yml_file["settings"]["base_uri"]
-    params       = yml_file["params"]
+    def to_s
+      body = ""
+      # TODO: respect HTTP Verb
+      #       make use of params
+      json_response = JSON.parse(Net::HTTP.get(URI.parse("http://#{@base_uri}#{@endpoint}")))
 
-    curl_services(web_services, base_uri)
-  end
+      body << @endpoint << "\n"
 
-  # TODO: respect HTTP Verb
-  #       make use of params
-
-  def self.curl_services(web_services, base_uri)
-    body = ""
-    web_services.each do |web_service|
-      json_response = JSON.parse(Net::HTTP.get(URI.parse("http://#{base_uri}#{web_service['endpoint']}")))
-
-      body << web_service["endpoint"] << "\n"
-      if web_service["params"]
+      if @params
         body << "Params: \n"
 
-        web_service["params"].each do |key, value|
+        @params.each do |key, value|
           body << "   #{key} \n"
-          web_service["params"][key].each do |k,v|
+          @params[key].each do |k,v|
             body << "    #{k}: #{v} \n"
           end
         end
-
       end
+
       body << "  response: \n"
       body << "    " << JSON.pretty_generate(json_response)
-      body << " \n "
+      body
+    end
+  end
+
+  def self.generate(path_to_web_services)
+    new.generate(path_to_web_services)
+  end
+
+  def generate(path_to_web_services)
+    @yml_file     = YAML.load_file(path_to_web_services)
+    @web_services = @yml_file["web_services"]
+    @base_uri     = @yml_file["settings"]["base_uri"]
+
+    curl_services
+  end
+
+private
+
+  def curl_services
+    body = ""
+
+    @web_services.each do |web_service|
+      service = Service.new(@base_uri, web_service)
+      body << service.to_s
+      body << " \n"
     end
 
     print body
   end
-
-
 end
 
-WebServiceDocumentor.generate_documentation("web_services.yml")
-
-# [{"verb"=>"get", "params"=>{"api_token"=>{"required"=>true, "description"=>"The API token that you got in your confirmation email", "example_value"=>"abc123"}}, "endpoint"=>"/users/1.json"}]
+WebServiceDocumentor.generate("web_services.yml")
